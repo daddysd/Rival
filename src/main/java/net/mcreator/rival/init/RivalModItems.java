6bd2bee8f5c37a9473ba848826a75e52b018cdfb
DeveloperKubilay <kubiss52@yahoo.com>
2025-03-31 23:15:24 +0300

    selam

--- src/main/java/net/mcreator/rival/init/RivalModItems.java
+++ src/main/java/net/mcreator/rival/init/RivalModItems.java
@@ -17,6 +17,7 @@ import net.mcreator.rival.RivalMod;
 public class RivalModItems {
 	public static final DeferredRegister<Item> REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, RivalMod.MODID);
 	public static final RegistryObject<Item> TEST = block(RivalModBlocks.TEST);
+	public static final RegistryObject<Item> BARINDIRMASIN = block(RivalModBlocks.BARINDIRMASIN);
 
 	// Start of user code block custom items
 	// End of user code block custom items
